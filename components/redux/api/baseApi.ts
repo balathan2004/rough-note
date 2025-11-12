89f339ff5fc258e799e5b1b7037bf63d9427685b
light <balathan2004@gmail.com>
2025-11-12 21:32:59 +0530

    db restructure finished & get_doc page finished

--- components/redux/api/baseApi.ts
+++ components/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://rough-note.vercel.app/api", // adjust as needed
+    baseUrl: "https://rough-note.vercel.app/api", 
     // no credentials here
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
